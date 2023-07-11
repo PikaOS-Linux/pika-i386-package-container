9d5580646bd5c75bfb8f56813a05d8647b754a3b
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-07-12 00:35:32 +0300

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -3,5 +3,6 @@ DIST=lunar ARCH=i386 debootstrap --arch=i386 lunar base_chroot || true
 rm -rf base_chroot/debootstrap
 rm -rf base_chroot/var
 mkdir base_chroot/var
+sudo tar -C ./base_chroot -c ./base_chroot.tgz
 
 
