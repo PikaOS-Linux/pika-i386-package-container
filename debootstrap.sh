4daa78f37527320d3e03202c5ea17c3435476e98
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-07-12 13:14:43 +0300

    initial creation

--- debootstrap.sh
+++ debootstrap.sh
@@ -3,7 +3,7 @@ DIST=lunar ARCH=i386 debootstrap --arch=i386 lunar base_chroot || true
 rm -rf base_chroot/debootstrap
 rm -rf base_chroot/var
 mkdir base_chroot/var
-chroot base_chroot /bin/bash -c "./chroot.sh"
+chroot ./base_chroot "./chroot.sh"
 sudo tar -czvf ./base_chroot.tgz ./base_chroot
 
 
