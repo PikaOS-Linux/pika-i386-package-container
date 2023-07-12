e2c0ce913036c24d409b9f270abadc1176b88b88
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-07-12 13:53:23 +0300

    add pika sources

--- debootstrap.sh
+++ debootstrap.sh
@@ -2,8 +2,10 @@
 set -e
 DIST=lunar ARCH=i386 debootstrap --arch=i386 lunar base_chroot || true
 rm -rf base_chroot/debootstrap
-chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y remove console-setup console-setup-linux"
+wget https://github.com/PikaOS-Linux/pkg-pika-sources/raw/lunar/pika-sources/etc/apt/sources.list.d/pika-apt.sources -o base_chroot/etc/apt/sources.list.d/pika-apt.sources
+wget https://github.com/PikaOS-Linux/pkg-pika-sources/raw/lunar/pika-sources/etc/apt/keyrings/pika-keyring.gpg.key -o base_chroot/etc/apt/keyrings/pika-keyring.gpg.key
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
+chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common sudo git bc gpg gpg-agent wget -y"
 chroot ./base_chroot /bin/bash -c "wget https://ppa.pika-os.com/dists/lunar/pika-sources.deb"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install ./pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew""
