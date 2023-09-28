3b4f2bacf2d6a46a26c191e84a8e12ee9bf3ff25
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-28 20:39:50 +0300

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -e
 # Run at 07/08/2023 19:40 UTC 3
-DIST=lunar ARCH=i386 debootstrap --arch=i386 lunar base_chroot || true
+DIST=mantic ARCH=i386 debootstrap --arch=i386 mantic base_chroot || true
 rm -rf base_chroot/debootstrap
-echo 'deb [trusted=yes] https://ppa.pika-os.com/ lunar main' > base_chroot/etc/apt/sources.list.d/pika-apt.list
+echo 'deb [trusted=yes] https://ppa.pika-os.com/ mantic main' > base_chroot/etc/apt/sources.list.d/pika-apt.list
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y install sudo git bc gpg gpg-agent wget -y"
-chroot ./base_chroot /bin/bash -c "wget https://ppa.pika-os.com/dists/lunar/pika-sources.deb"
+chroot ./base_chroot /bin/bash -c "wget https://ppa.pika-os.com/dists/mantic/pika-sources.deb"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install ./pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew""
 rm -rf base_chroot/etc/apt/sources.list.d/pika-apt.list
 chroot ./base_chroot /bin/bash -c "rm -rf ./pika-sources.deb"
