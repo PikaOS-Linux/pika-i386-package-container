1291215f39eea77421185b7288230f5d94def75b
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-29 12:36:57 +0300

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -7,7 +7,7 @@ echo 'deb [trusted=yes] https://ppa.pika-os.com/ pikauwu main' > base_chroot/etc
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -y install sudo git bc gpg gpg-agent wget -y"
-chroot ./base_chroot /bin/bash -c "wget https://ppa.pika-os.com/dists/mantic/pika-sources.deb"
+chroot ./base_chroot /bin/bash -c "wget https://ppa.pika-os.com/dists/pikauwu/pika-sources.deb"
 chroot ./base_chroot /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install ./pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew""
 rm -rf base_chroot/etc/apt/sources.list.d/pika-apt.list
 chroot ./base_chroot /bin/bash -c "rm -rf ./pika-sources.deb"
